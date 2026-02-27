ed5de785cfa2efeb31b86a1c0b13204f1b33cc18
chagelo <changeto104@gmail.com>
2026-02-27 21:02:45 +0800

    fix hashtable_oa

--- hashtable_oa/benches/bench.rs
+++ hashtable_oa/benches/bench.rs
@@ -1,25 +1,21 @@
-#![feature(test)]
-extern crate test;
-
-use test::Bencher;
+use bencher::{benchmark_group, benchmark_main, Bencher};
 
 use hashtable::HashTable;
 
-fn benchmark() {
-    let mut hash = HashTable::<usize, usize>::new();
-
-    for _ in 0..1000 {
-        let key = rand::random::<usize>();
-        if let Some(value) = hash.get_mut(&key) {
-            *value += 1;
-        } else {
-            hash.insert(key, 1);
-        }
-    }
-    // hash.debug_dump();
-}
-
-#[bench]
 fn benchmark_test(b: &mut Bencher) {
-    b.iter(|| benchmark());
+    b.iter(|| {
+        let mut hash = HashTable::<usize, usize>::new();
+
+        for _ in 0..1000 {
+            let key = rand::random::<usize>();
+            if let Some(value) = hash.get_mut(&key) {
+                *value += 1;
+            } else {
+                hash.insert(key, 1);
+            }
+        }
+    });
 }
+
+benchmark_group!(benches, benchmark_test);
+benchmark_main!(benches);
